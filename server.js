113521021faf12291b5372d7f3c8233485cfd533
Basu Pandey <basupandey1234@gmail.com>
2026-06-30 00:47:01 +0530

    3

--- server.js
+++ server.js
@@ -100,20 +100,9 @@ app.get("*", (req, res) => {
   app.use(notFoundMiddleware);
   app.use(errorHandlerMiddleware);
   
-  const port = process.env.PORT || 5000;
-  
-  const start = async () => {
-    try {
-      await connectDB(process.env.MONGO_URL);
-      app.listen(port, () =>
-        console.log(`Server is listening on port ${port}...`)
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  start();
+await connectDB(process.env.MONGO_URL);
+
+export default app;
 
 // // Schedule health check
 // cron.schedule("*/14 * * * *", async () => {
